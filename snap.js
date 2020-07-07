f7521ea6e4da3caabd80616ed300ba74c2abb5ce
Namrata-m13 <43001712+Namrata-m13@users.noreply.github.com>
2020-07-07 09:45:55 +0530

    Update snap.js

--- snap.js
+++ snap.js
@@ -2,6 +2,9 @@ const PercyScript = require("@percy/script")
 
 PercyScript.run(async(page, percySnapshot)=>{
     await page.goto("https://www.google.com")
+    
+    await percySnapshot("Verify Webpage",{widths: [768, 992, 1200]})
+
     await page.waitFor("[name='q']")
     await page.type("[name='q']","BrowserStack")
 
